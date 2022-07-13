d38723e71b830d1cc6026cbaf9a2822bb060ab14
codeanticode <andres.colubri@gmail.com>
2022-07-13 00:35:41 -0400

    thin native libraries

--- scripts/pack_macosx_libs.sh
+++ scripts/pack_macosx_libs.sh
@@ -19,25 +19,48 @@ else
 fi
 
 gst_folder=/Library/Frameworks/GStreamer.framework/Versions/1.0/lib
-lib_folder=../library/macos-universal
+lib_folder_univ=../library/macos-universal
+lib_folder_x86_64=../library/macos-x86_64
+lib_folder_aarch64=../library/macos-aarch64
 
 echo "Copying base gstreamer libs..."
-mkdir -p ${lib_folder}
-cp ${gst_folder}/*.dylib ${lib_folder}
+mkdir -p ${lib_folder_univ}
+cp ${gst_folder}/*.dylib ${lib_folder_univ}
 
 echo "Relocating dependencies in base libs..."
-./macosx_relocator.py ${lib_folder} ${dep_path} "@loader_path/"
+./macosx_relocator.py ${lib_folder_univ} ${dep_path} "@loader_path/"
 
 echo "Copying gstreamer plugins..."
-mkdir -p ${lib_folder}/gstreamer-1.0
-cp ${gst_folder}/gstreamer-1.0/* ${lib_folder}/gstreamer-1.0
+mkdir -p ${lib_folder_univ}/gstreamer-1.0
+cp ${gst_folder}/gstreamer-1.0/* ${lib_folder_univ}/gstreamer-1.0
 
 # Silence runtime error from these plugins
-rm -f ${lib_folder}/gstreamer-1.0/libgsthls.so
-rm -f ${lib_folder}/gstreamer-1.0/libgstopenjpeg.so
+# rm -f ${lib_folder_univ}/gstreamer-1.0/libgsthls.so
+# rm -f ${lib_folder_univ}/gstreamer-1.0/libgstopenjpeg.so
+rm -f ${lib_folder_univ}/gstreamer-1.0/libgstsrt.dylib
 
 echo "Relocating dependencies in gstreamer plugins..."
-./macosx_relocator.py ${lib_folder}/gstreamer-1.0 ${dep_path} "@loader_path/../"
+./macosx_relocator.py ${lib_folder_univ}/gstreamer-1.0 ${dep_path} "@loader_path/../"
 
 echo "Removing unused dependencies..."
 ./macosx_remove_extra_libs.py
+
+echo "Thinning x86_64 and aarch64 native libraries..."
+
+mkdir -p ${lib_folder_x86_64}
+mkdir -p ${lib_folder_aarch64}
+for file in ${lib_folder_univ}/*.dylib; do 
+  fn="$(basename ${file})"
+  lipo ${file} -thin x86_64 -output ${lib_folder_x86_64}/${fn};
+  lipo ${file} -thin arm64 -output ${lib_folder_aarch64}/${fn};
+done
+
+mkdir -p ${lib_folder_x86_64}/gstreamer-1.0
+mkdir -p ${lib_folder_aarch64}/gstreamer-1.0
+for file in ${lib_folder_univ}/gstreamer-1.0/*.dylib; do 
+  fn="$(basename ${file})"
+  lipo ${file} -thin x86_64 -output ${lib_folder_x86_64}/gstreamer-1.0/${fn};
+  lipo ${file} -thin arm64 -output ${lib_folder_aarch64}/gstreamer-1.0/${fn};
+done
+
+echo "Done."
