b5647722dbb9a61550fa91e7b29bc843c820f036
codeanticode <andres.colubri@gmail.com>
2022-08-22 22:49:46 -0400

    windows pack script assumes MSVC

--- scripts/pack_windows_libs.bat
+++ scripts/pack_windows_libs.bat
@@ -1,46 +1,11 @@
 SET gst_folder=C:\gstreamer
-SET gst_toolchain=
+SET gst_toolchain=msvc
 SET lib_folder=..\library
 
 echo "Copying base gstreamer libs..."
-md %lib_folder%\windows64 %lib_folder%\windows32
-copy %gst_folder%\1.0\%gst_toolchain%x86_64\bin\*.dll %lib_folder%\windows64
-copy %gst_folder%\1.0\%gst_toolchain%x86\bin\*.dll %lib_folder%\windows32
+md %lib_folder%\windows-amd64
+copy %gst_folder%\1.0\%gst_toolchain%_x86_64\bin\*.dll %lib_folder%\windows-amd64
 
 echo "Copying gstreamer plugins..."
-md %lib_folder%\windows64\gstreamer-1.0 %lib_folder%\windows32\gstreamer-1.0
-copy %gst_folder%\1.0\%gst_toolchain%x86_64\lib\gstreamer-1.0\*.dll %lib_folder%\windows64\gstreamer-1.0
-copy %gst_folder%\1.0\%gst_toolchain%x86\lib\gstreamer-1.0\*.dll %lib_folder%\windows32\gstreamer-1.0
-
-echo "Remove broken plugins..."
-del %lib_folder%\windows64\gstreamer-1.0\libgsta52dec.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstamrnb.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstamrwbdec.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstassrender.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstdtsdec.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstdvdread.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstges.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstlibvisual.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstmms.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstresindvd.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstrtmp.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstsoundtouch.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstvoaacenc.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstwebrtcdsp.dll /f
-del %lib_folder%\windows64\gstreamer-1.0\libgstx264.dll /f
-
-del %lib_folder%\windows32\gstreamer-1.0\libgsta52dec.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstamrnb.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstamrwbdec.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstassrender.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstdtsdec.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstdvdread.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstges.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstlibvisual.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstmms.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstresindvd.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstrtmp.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstsoundtouch.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstvoaacenc.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstwebrtcdsp.dll /f
-del %lib_folder%\windows32\gstreamer-1.0\libgstx264.dll /f
+md %lib_folder%\windows-amd64\gstreamer-1.0
+copy %gst_folder%\1.0\%gst_toolchain%_x86_64\lib\gstreamer-1.0\*.dll %lib_folder%\windows-amd64\gstreamer-1.0
